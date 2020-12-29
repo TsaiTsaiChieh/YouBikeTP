39a58db599053e197b791237ad3628df83fd5d2d
d0177381@gmail.com <>
2020-12-30 01:09:22 +0800

    [Add] user API

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const { appPort } = process.env;
+const bodyParser = require('body-parser');
 
 const app = express();
+// parse application/json
+app.use(bodyParser.json());
 
 app.use('/connection_mysql', require('./src/routes/connection'));
 app.use('/migration', require('./src/routes/migration'));
+app.use('/user', require('./src/routes/user'));
 
 app.listen(appPort, function() {
   console.log(`YouBike on port: ${appPort}`);
