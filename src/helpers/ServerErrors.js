b6cdc81f7b8936aa5b51f1951a7f236776a222a7
d0177381@gmail.com <>
2020-12-29 17:39:30 +0800

    [Add] server erro classes

---
+++ src/helpers/ServerErrors.js
@@ -0,0 +1,30 @@
+const { INTERNAL_SERVER_ERROR } = require('http-status');
+
+class ExtendableError extends Error {
+  constructor(message, status, isPublic, code) {
+    super(message);
+    this.message = message;
+    this.name = this.constructor.name;
+    this.status = status;
+    this.isPublic = isPublic;
+    this.code = code;
+    Error.captureStackTrace(this, this.constructor.name);
+  }
+}
+
+class GetDataFromURL extends ExtendableError {
+  constructor(message, status = 500000, isPublic = true, code = INTERNAL_SERVER_ERROR) {
+    super(message, status, isPublic, code);
+  }
+}
+
+class MySQLError extends ExtendableError {
+  constructor(message, status = 500001, isPublic = true, code = INTERNAL_SERVER_ERROR) {
+    super(message, status, isPublic, code);
+  }
+}
+
+module.exports = {
+  GetDataFromURL,
+  MySQLError
+};
