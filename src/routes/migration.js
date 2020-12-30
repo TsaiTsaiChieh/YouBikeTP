b3068ef687fbe4502e1a2dcf138be8b2a086c5eb
d0177381@gmail.com <>
2020-12-30 14:28:51 +0800

    [Change] comment sync DB API

--- src/routes/migration.js
+++ src/routes/migration.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { YouBike } = require('../schemas/YouBike');
 const { Region } = require('../schemas/Region');
 const { User } = require('../schemas/User');
+const { Comment } = require('../schemas/Comment');
 const { INTERNAL_SERVER_ERROR } = require('http-status');
 const regions = require('../json/regions.json');
 
@@ -11,8 +12,10 @@ const regions = require('../json/regions.json');
 //     await YouBike.sync();
 //     await Region.sync();
 //     await User.sync();
+//     await Comment.sync();
 //     return res.send('Sync DB.');
 //   } catch (err) {
+//     console.log(err);
 //     return res.status(INTERNAL_SERVER_ERROR).json({
 //       message: 'Unable to sync DB',
 //       error: err
