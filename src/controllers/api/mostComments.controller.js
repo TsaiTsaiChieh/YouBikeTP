ebf84a23a028c82509c1832260f7c82683f5acc2
d0177381@gmail.com <>
2020-12-30 20:02:56 +0800

    [Add] GET most comment API

---
+++ src/controllers/api/mostComments.controller.js
@@ -0,0 +1,27 @@
+const { BAD_REQUEST } = require('http-status');
+const ajv = require('../../helpers/ajvUtil');
+const model = require('../../models/api/mostComment.model');
+
+async function controller(req, res) {
+  const schema = {
+    type: 'object',
+    properties: {
+      page: {
+        type: 'number',
+        default: 1
+      }
+    }
+  };
+  req.query.page = parseInt(req.query.page);
+  const valid = ajv.validate(schema, req.query);
+  if (!valid) return res.status(BAD_REQUEST).json(ajv.errors);
+  try {
+    return res.json(await model(req.query));
+  } catch (err) {
+    return res.status(err.code).json(err.isPublic
+      ? { error: err.name, devcode: err.status, message: err.message }
+      : err.code);
+  }
+}
+
+module.exports = controller;
---
+++ src/models/api/mostComment.model.js
@@ -0,0 +1,37 @@
+const { mysql } = require('../../configs/mysqlSetting');
+const ServerErrors = require('../../helpers/ServerErrors');
+const limit = 3;
+const { QueryTypes } = require('sequelize');
+
+async function model(args) {
+  try {
+    const result = await getMostCommentSortBySites(args);
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+async function getMostCommentSortBySites(args) {
+  try {
+    const offset = (args.page - 1) * limit;
+    const result = await mysql.query(
+      `SELECT YouBikes.id, YouBikes.name ,parking_num, available_num, address, address_en,
+              Regions.name AS regionName, Regions.name_en AS regionNameEn,
+              COUNT(*) AS comment_count FROM Comments
+    LEFT JOIN YouBikes ON YouBikes.id = Comments.site_id
+    LEFT JOIN Regions ON Regions.id = YouBikes.region_id
+     GROUP BY site_id
+     ORDER BY comment_count DESC, site_id
+        LIMIT :limit OFFSET :offset`, {
+        replacements: { limit, offset },
+        type: QueryTypes.SELECT,
+        raw: true
+      });
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
+}
+
+module.exports = model;
