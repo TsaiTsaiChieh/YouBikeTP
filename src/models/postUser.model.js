cbdf64eb1a8a0524b33b482c37c0090d98fb468e
d0177381@gmail.com <>
2020-12-30 12:13:46 +0800

    [Add] generate token function and save to DB

--- src/models/postUser.model.js
+++ src/models/postUser.model.js
@@ -3,13 +3,15 @@ const { User } = require('../schemas/User');
 const ClientErrors = require('../helpers/ClientErrors');
 const bcrypt = require('bcrypt');
 const saltRounds = parseInt(process.env.saltRounds);
+const { getToken } = require('../helpers/tokenModules');
 
 async function model(args) {
   try {
     await checkEmailIsDuplicated(args.email);
-    args.password = await getHash(args.password);
-    await createUserData(args);
-    return Promise.resolve('註冊成功');
+    args.password = await passwordAddSalt(args.password);
+    args = getToken(args);
+    const result = await createUserData(args);
+    return Promise.resolve(result);
   } catch (err) {
     return Promise.reject(err);
   }
@@ -25,7 +27,7 @@ async function checkEmailIsDuplicated(email) {
   }
 }
 
-async function getHash(password) {
+async function passwordAddSalt(password) {
   try {
     const hash = await bcrypt.hash(password, saltRounds);
     return Promise.resolve(hash);
@@ -37,7 +39,8 @@ async function getHash(password) {
 async function createUserData(args) {
   try {
     const result = await User.create(args);
-    return Promise.resolve(result);
+    const { id, name, email, token, token_expired } = result;
+    return Promise.resolve({ user: { id, name, email }, token: { access_token: token, token_expired } });
   } catch (err) {
     return Promise.reject(new ServerErrors.MySQLError(err.stack));
   }
