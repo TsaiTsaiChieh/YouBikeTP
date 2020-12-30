f0bdbed8fc708da0c16b7788a0abcdccd93f6135
d0177381@gmail.com <>
2020-12-30 12:11:26 +0800

    [Change] User table schema by adding token, token_expired and tune password string length

--- src/schemas/User.js
+++ src/schemas/User.js
@@ -5,7 +5,9 @@ const User = mysql.define('User', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, allowNull: false, primaryKey: true, comment: '使用者編號' },
   name: { type: Sequelize.STRING(16), allowNull: false, comment: '使用者姓名' },
   email: { type: Sequelize.STRING(64), allowNull: false, comment: '使用者帳號' },
-  password: { type: Sequelize.STRING(32), allowNull: false, comment: '使用者加鹽後的密碼' }
+  password: { type: Sequelize.STRING(72), allowNull: false, comment: '使用者加鹽後的密碼' },
+  token: { type: Sequelize.STRING(64), comment: '使用者 token' },
+  token_expired: { type: Sequelize.DATE, comment: 'token 過期時間' }
 }, {
   indexes: [{ fields: ['email'] }]
 });
