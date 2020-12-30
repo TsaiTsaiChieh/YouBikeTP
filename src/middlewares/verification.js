6ee03f7ee29173f235b027c7209df90787890074
d0177381@gmail.com <>
2020-12-30 16:42:01 +0800

    [Add] verification middleware

---
+++ src/middlewares/verification.js
@@ -0,0 +1,43 @@
+const { UNAUTHORIZED } = require('http-status');
+const ServerErrors = require('../helpers/ServerErrors');
+const { User } = require('../schemas/User');
+const ClientErrors = require('../helpers/ClientErrors');
+const moment = require('moment');
+
+async function verification(req, res, next) {
+  try {
+    const now = Date.now();
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(UNAUTHORIZED).send('Should login');
+    const token = authorization.replace('Bearer ', '');
+    const userInfo = await findUser(token);
+    await checkTokenIsExpired(now, userInfo.token_expired);
+    req.user = userInfo;
+    next();
+  } catch (err) {
+    return res.status(UNAUTHORIZED).send(err);
+  }
+}
+
+async function findUser(token) {
+  try {
+    const result = await User.findOne(
+      {
+        attributes: ['id', 'name', 'email', 'token', 'token_expired'],
+        where: { token },
+        raw: true
+      });
+    if (!result) return Promise.reject(new ClientErrors.TokenNotFound());
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
+}
+
+async function checkTokenIsExpired(now, token_expired) {
+  const tokenExpired = moment(token_expired).valueOf();
+  if (now > tokenExpired) return Promise.reject(new ClientErrors.TokenExpired());
+  else return Promise.resolve();
+}
+
+module.exports = { verification };
