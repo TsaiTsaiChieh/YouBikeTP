d5097c2842c80b526f9f83a236129e5c85fda9b4
d0177381@gmail.com <>
2020-12-30 13:22:09 +0800

    [Add] delete token API

--- src/routes/token.js
+++ src/routes/token.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', require('../controllers/login.controller'));
+router.delete('/', require('../controllers/logout.controller'));
 
 module.exports = router;
