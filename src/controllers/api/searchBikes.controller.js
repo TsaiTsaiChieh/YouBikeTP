09b84ebd4f3feae86c81f65cfbbc42beb971623c
d0177381@gmail.com <>
2020-12-30 23:01:26 +0800

    [Add] search bike API

---
+++ src/controllers/api/searchBikes.controller.js
@@ -0,0 +1,28 @@
+const { BAD_REQUEST } = require('http-status');
+const ajv = require('../../helpers/ajvUtil');
+const model = require('../../models/api/searchBikes.model');
+
+async function controller(req, res) {
+  const schema = {
+    type: 'object',
+    required: ['num'],
+    properties: {
+      num: {
+        type: 'number',
+        default: 0
+      }
+    }
+  };
+  req.query.num = parseInt(req.query.num);
+  const valid = ajv.validate(schema, req.query);
+  if (!valid) return res.status(BAD_REQUEST).json(ajv.errors);
+  try {
+    return res.json(await model(req.query));
+  } catch (err) {
+    return res.status(err.code).json(err.isPublic
+      ? { error: err.name, devcode: err.status, message: err.message }
+      : err.code);
+  }
+}
+
+module.exports = controller;
---
+++ src/models/api/searchBikes.model.js
@@ -0,0 +1,22 @@
+const ServerErrors = require('../../helpers/ServerErrors');
+const { YouBike } = require('../../schemas/YouBike');
+
+async function model(args) {
+  try {
+    const result = await searchSiteByBikes(args.num);
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+async function searchSiteByBikes(num) {
+  try {
+    const result = await YouBike.findAll({ where: { available_num: num }, raw: true });
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(new ServerErrors.MySQLError(err.stack));
+  }
+}
+
+module.exports = model;
