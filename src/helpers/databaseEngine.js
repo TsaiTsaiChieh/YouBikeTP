cb6c9fad65de52a1da14ac2ce854c602aef865c4
d0177381@gmail.com <>
2020-12-31 14:55:06 +0800

    [Add] getDataFromAPI function

--- src/helpers/databaseEngine.js
+++ src/helpers/databaseEngine.js
@@ -1,6 +1,8 @@
 const ServerErrors = require('./ServerErrors');
 const { Comment } = require('../schemas/Comment');
 const ClientErrors = require('./ClientErrors');
+const axios = require('axios');
+const { API } = process.env;
 
 async function findCommitBelongToCertainUser(args) {
   try {
@@ -12,7 +14,17 @@ async function findCommitBelongToCertainUser(args) {
   }
 };
 
-module.exports = {
-  findCommitBelongToCertainUser
+async function getDataFromAPI() {
+  try {
+    const { data } = await axios.get(API);
+    return Promise.resolve(data.retVal);
+  } catch (err) {
+    console.log(err);
+    return Promise.reject(new ServerErrors.GetDataFromAPI(err.stack));
+  }
 }
-;
+
+module.exports = {
+  findCommitBelongToCertainUser,
+  getDataFromAPI
+};
